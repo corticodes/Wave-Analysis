be7c7ae7c202e6c4a87564e212ba9d5a368f5c42
MSI ComputeServer <corticodes@gmail.com>
2020-02-13 14:57:14 +0200

    Change description

--- small functions and plots/getSpikePhase.m
+++ small functions and plots/getSpikePhase.m
@@ -1,8 +1,21 @@
 function [spikePhase] = getSpikePhase(TIC,HTangle,timeSequence)
 %GETSPIKEPHASE returns the phase in which the channel's oscilation was 
-%when the neuron fired
-%   TIC is the combined array of t,ic. Every column is a spike, and rows
-%   are time,channel,neuron number.
+%when the neuron fired. This function can also be used for any other
+%feature of the data simply by changing the input to the relevant variable
+%   Input:
+%       TIC is the combined array of t,ic. Every column is a spike, and rows
+%       are time,channel,neuron number.
+%       HTangle (nChXnSequenceSamples) is the phase of oscilation in each
+%       channel. To get other type of data for for the neurons simply enter
+%       the relevant data: Hilbert amplitude (HTabs), filtered data
+%       (FDsequence) etc. It just needs to be in the right format (format
+%       of HTangle)
+%       timeSequence (1XnSequenceSamples) - the timestamp for every sample 
+%       in HTangle (as recieved by getDataSequence)
+%   Output:
+%       spikePhase (1XnSpikes) is the phase (or other type of data given in
+%       HTangle) of the channel in which the spike occured, at the time it
+%       occured
 
 nSpikes=size(TIC,2);
 % nNeurons=TIC(3,:);
