15cb6a5381dbc23c27bd1bb00f1e6f17aedf4e75
YuvalOrhser <u.vorsher@gmail.com>
2020-04-23 12:35:53 +0300

    Change movie spikes to red circles

---
+++ small functions and plots/createBinaryCircle.m
@@ -0,0 +1,21 @@
+function ringPixels = createBinaryCircle(imageSizeX,imageSizeY,centerX,centerY,radius,thickness,plotCircle)
+%CREATEBINARYCIRCLE creates logical imageSizeY by imageSizeX matrix with
+%ones on the circumfrance of a circle located at centerX,centerY 
+%   plotCircle is an option to also plot the circle
+
+[columnsInImage, rowsInImage] = meshgrid(1:imageSizeX, 1:imageSizeY);
+% Next create the circle in the image.
+
+outerRadius = radius+thickness;
+array2D = (rowsInImage - centerY).^2 ...
+    + (columnsInImage - centerX).^2;
+ringPixels = array2D >= radius.^2 & array2D <= outerRadius.^2;
+% ringPixels is a 2D "logical" array.
+% Now, display it.
+if plotCircle
+    image(ringPixels) ;
+    colormap([0 0 0; 1 1 1]);
+    title('Binary Image of a Ring', 'FontSize', 25);
+end
+end
+
--- small functions and plots/exportVideo.m
+++ small functions and plots/exportVideo.m
@@ -24,7 +24,7 @@ function [] = exportVideo(data,videoDir,frameRate,pixelsPerChannel,varargin)
 
 spikeFrameLength=10;
 particleSize=10;
-pixelsPerSpike=1;
+pixelsPerSpike=pixelsPerChannel(1);
 
 frameHeight=size(data,1);
 frameWidth=size(data,2);
@@ -37,7 +37,7 @@ end
 
 if exist('spikeCoordinates','var')
    markSpikes=1;
-   maxGrayscale=200; %so brightest pixel will be spike, brighter than max voltage
+   maxGrayscale=215; %so brightest pixel will be spike, brighter than max voltage
 else
    markSpikes=0;
    maxGrayscale=255; %brightest pixel is highest voltage
@@ -46,22 +46,24 @@ end
 scaledData=data-min(data(:));
 scaledData=scaledData/max(scaledData(:));
 dataFramesGS=round(scaledData*maxGrayscale);
-if markSpikes
-    for i=1:size(spikeCoordinates,1)
-        dataFramesGS(spikeCoordinates(i,1):min(spikeCoordinates(i,1)+pixelsPerSpike-1,frameHeight),...
-            spikeCoordinates(i,2):min(spikeCoordinates(i,2)+pixelsPerSpike-1,frameWidth)...
-            ,spikeCoordinates(i,3):min(spikeCoordinates(i,3)+spikeFrameLength-1,nFrames))=255;
-    end
-end
 
 %smooth video
 dataFramesGS_smoothed=zeros(pixelsPerChannel(1)*frameHeight,pixelsPerChannel(2)*frameWidth,nFrames);
 
 spaced=imresize(dataFramesGS,[pixelsPerChannel(1)*frameHeight,pixelsPerChannel(2)*frameWidth]);
-
+tic
 for i=1:nFrames
     dataFramesGS_smoothed(:,:,i) = imgaussfilt(spaced(:,:,i),pixelsPerChannel/3,'FilterSize',pixelsPerChannel);
 end
+toc,disp('end of smoothing')
+
+% if markSpikes
+%     for i=1:size(spikeCoordinates,1)
+%         dataFramesGS(spikeCoordinates(i,1):min(spikeCoordinates(i,1)+pixelsPerSpike-1,frameHeight),...
+%             spikeCoordinates(i,2):min(spikeCoordinates(i,2)+pixelsPerSpike-1,frameWidth)...
+%             ,spikeCoordinates(i,3):min(spikeCoordinates(i,3)+spikeFrameLength-1,nFrames))=255;
+%     end
+% end
 
 %add particlePath if given
 if exist('particlePath','var')
@@ -72,19 +74,78 @@ if exist('particlePath','var')
     end
 end
 
-%renormalize
-dataFramesGS_smoothed=uint8(round(255*(dataFramesGS_smoothed/max(dataFramesGS_smoothed(:)))));
+% %renormalize
+% dataFramesGS_smoothed=uint8(round(maxGrayscale*(dataFramesGS_smoothed/max(dataFramesGS_smoothed(:)))));
+
+
+if markSpikes
+%     dataFramesGS_smoothedSIZE=size(dataFramesGS_smoothed);
+%     dataFramseRGB=ones(dataFramesGS_smoothedSIZE(1),dataFramesGS_smoothedSIZE(2),3,dataFramesGS_smoothedSIZE(3));
+    
+    R=dataFramesGS_smoothed;
+    GB=dataFramesGS_smoothed;
+    for i=1:size(spikeCoordinates,1)
+        tic
+%         dataFramesGS(spikeCoordinates(i,1):min(spikeCoordinates(i,1)+pixelsPerSpike-1,frameHeight),...
+%             spikeCoordinates(i,2):min(spikeCoordinates(i,2)+pixelsPerSpike-1,frameWidth)...
+%             ,spikeCoordinates(i,3):min(spikeCoordinates(i,3)+spikeFrameLength-1,nFrames))=255;
+        
+        %create hollow circles
+%         [I,J] = find(createBinaryCircle(pixelsPerChannel(2)*frameWidth,pixelsPerChannel(1)*frameHeight,pixelsPerChannel(1)*spikeCoordinates(i,2),pixelsPerChannel(1)*spikeCoordinates(i,1),pixelsPerChannel(1),1,0));
+        spikeCircle=createBinaryCircle(pixelsPerChannel(2)*frameWidth,pixelsPerChannel(1)*frameHeight,pixelsPerChannel(1)*spikeCoordinates(i,2),pixelsPerChannel(1)*spikeCoordinates(i,1),pixelsPerSpike/4,1,0);
+        spikeFrames=spikeCoordinates(i,3):min(spikeCoordinates(i,3)+spikeFrameLength-1,nFrames);
+% %         for j=1:numel(spikeFrames)
+% %             dataFramesGS(:,:,j)=dataFramesGS(:,:,j).*~spikeCircle+255*spikeCircle;
+% %         end
+        R(:,:,spikeFrames)=R(:,:,spikeFrames).*repmat(~spikeCircle,1,1,numel(spikeFrames))+255*repmat(spikeCircle,1,1,numel(spikeFrames));
+        GB(:,:,spikeFrames)=GB(:,:,spikeFrames).*repmat(~spikeCircle,1,1,numel(spikeFrames));
+%         tic
+%         dataFramesGS_smoothed(:,:,spikeFrames)=dataFramesGS_smoothed(:,:,spikeFrames).*repmat(~spikeCircle,1,1,numel(spikeFrames))+255*repmat(spikeCircle,1,1,numel(spikeFrames));
+%         toc,disp('mark 255')
+%         %convert 2 RGB
+%         tic
+%         toc,disp('create RGB r dim')
+%         tic
+%         greenNblue=dataFramesGS_smoothed;
+%         greenNblue(greenNblue==255)=0;
+%         toc,disp('create greenNblue and assin 0 to 255')
+        
+%         for j=2:3
+%             tic
+%             dataFramseRGB(:,:,j,:)=greenNblue;
+%             toc,disp('end of loop j')
+%         end
+%         toc,disp('end of loop i')
+       
+%         dataFramsRGB(:,:,2,spikeCoordinates(i,3))=dataFramsRGB(:,:,2,spikeCoordinates(i,3)).*(~spikeCircle);
+%         dataFramsRGB(:,:,3,spikeCoordinates(i,3))=dataFramsRGB(:,:,3,spikeCoordinates(i,3)).*(~spikeCircle);
+        toc, disp('end of loop i')
+    end
+    dataFramseRGB(:,:,1,:)=R;
+    dataFramseRGB(:,:,2,:)=GB;
+    dataFramseRGB(:,:,3,:)=GB;
+    data2export=dataFramseRGB;
+else
+    data2export=dataFramesGS_smoothed;
+end
 
 % dataFramesRGB=
 
-dataVideo = VideoWriter(videoDir,'Grayscale AVI');
+% dataVideo = VideoWriter(videoDir,'Grayscale AVI');
+
+dataVideo = VideoWriter(videoDir);
 dataVideo.FrameRate=frameRate;
 open(dataVideo);
 
-
-for i=1:nFrames
-   writeVideo(dataVideo,dataFramesGS_smoothed(:,:,i)); 
-end
+tic
+data2export_uint8=uint8(data2export);
+toc,disp('end of uint8 conversion')
+tic
+writeVideo(dataVideo,data2export_uint8); 
+toc,disp('end write video')
+% for i=1:nFrames
+%    writeVideo(dataVideo,dataFramesGS_smoothed(:,:,i)); 
+% end
 
 close(dataVideo);
 
