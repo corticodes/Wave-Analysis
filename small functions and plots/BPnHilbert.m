f1cb533b5893aa55637aa5dbee4b8dfa22b0f7ed
YuvalOrhser <u.vorsher@gmail.com>
2020-05-31 16:48:08 +0300

    Allow Also Low and High Pass (beside BP)

--- small functions and plots/BPnHilbert.m
+++ small functions and plots/BPnHilbert.m
@@ -8,10 +8,14 @@ function [FD,HT,HTabs,HTangle] = BPnHilbert(data,bandpass,varargin)
 %   where FD (chXtrialXsample) is the filtered data, HT is the complex hilbert analytic,
 %   HTabs is the complex magnitude of the analytic (the envelope) and HT
 %   angle is the hilbert phase.
+%   For Low/High pass use bandpass=[0 LP] or bandpass=[HP 0] respectively
 %   Varargin:
 %       -   cutwidths: cutwidths(1) to the right of bandpass(1), 
 %       cutwidths(2) to the left of bandpass(2). Default is [2 2]
 %       -   SamplingFrequency - self explanatory
+
+%TODO: QA high pass
+
 
 cutwidths=[2 2]; %
 SamplingFrequency=20000;
@@ -21,11 +25,19 @@ end
 
 F=filterData(SamplingFrequency);
 F.padding=true;
-F.lowPassStopCutoff=bandpass(2)+cutwidths(2); %low-pass cutoff frequency
-F.lowPassPassCutoff=bandpass(2);
-F.highPassStopCutoff=bandpass(1)-cutwidths(1); %high-pass cutoff frequency
-F.highPassPassCutoff=bandpass(1);
-F=F.designBandPass;
+if all(bandpass) %no zeros
+    F.lowPassStopCutoff=bandpass(2)+cutwidths(2); %low-pass cutoff frequency
+    F.lowPassPassCutoff=bandpass(2);
+    F.highPassStopCutoff=bandpass(1)-cutwidths(1); %high-pass cutoff frequency
+    F.highPassPassCutoff=bandpass(1);
+    F=F.designBandPass;
+elseif bandpass(1)==0 %low pass filter
+    F.lowPassCutoff=bandpass(2);
+    F=F.designLowPass;
+else
+    F.highPassCutoff=bandpass(1);
+    F=F.designHighPass;        
+end
 
 FD=F.getFilteredData(data);
 
