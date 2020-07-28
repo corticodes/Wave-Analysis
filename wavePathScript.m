b59e83e751e351557d632b8ce0ea843ec6696524
YuvalOrhser <u.vorsher@gmail.com>
2020-07-28 10:42:44 +0300

    add wavePath analysis to gauss wave

--- wavePathScript.m
+++ wavePathScript.m
@@ -4,8 +4,8 @@
 layoutSize=12;
 gaussSigma=3;
 pulseFrames=100;
-% distInSigmas=[2.5 2.5];
-distInSigmas=[1.5 1.5];
+distInSigmas=[2.5 2.5];
+% distInSigmas=[1.5 1.5];
 tempOverlapInPulseFrames=0.7;
 En=reshape(1:(layoutSize^2),layoutSize,layoutSize);
 
@@ -27,9 +27,12 @@ plotCrossingsPhysical(crossings{1},startEndWave,flipud(En),hilbertAmps{1},'Units
 spikesCoordinates=simulateSpikes(waveData,0.01);
 plotWaveSpikes(spikesCoordinates,size(En));
 
-waveCenterPath = drawWavePath(crossings{1},hilbertAmps{1},startEndWave,En);
+waveCenterPath = drawWavePath(crossings{1},hilbertAmps{1},startEndWave,flipud(En));
 
+% exportVideo(waveData,'\\sil2\Literature\Projects\corplex\progress reports\meetings\next\simulations\correlation coefficient\Two Gauss prob 0.01 with Wave Path and Spikes',30,pixelsPerChannel,'particlePath',waveCenterPath,'spikeCoordinates',spikesCoordinates);
+% exportVideo(waveData,'E:\Yuval\Analysis\DataAnalysis\waves and spike sorting\Gereal testings exports etc\Two Gauss prob 0.01 with Wave Path and Spikes',30,pixelsPerChannel,'particlePath',waveCenterPath,'spikeCoordinates',spikesCoordinates);
 exportVideo(waveData,'\\sil2\Literature\Projects\corplex\progress reports\meetings\next\simulations\correlation coefficient\Two Gauss prob 0.01 with Wave Path and Spikes',30,pixelsPerChannel,'particlePath',waveCenterPath,'spikeCoordinates',spikesCoordinates);
+exportVideo(waveData,'\\sil2\Literature\Projects\corplex\progress reports\meetings\next\Velocity profiles\Two Gauss Wave Path and Spikes',30,pixelsPerChannel,'particlePath',waveCenterPath);
 
 %visualize spike and waveCenterPath in 3d
 nSamples=size(waveCenterPath,1);
@@ -41,6 +44,7 @@ h=plotWaveSpikes([waveCenterPath,t';spikesCoordinates],size(En),plottingInd);
 
 %check spatial correlation
 rvc=rvcoeff(spikesCoordinates(:,1:2),waveCenterPath(spikesCoordinates(:,3),:))
+dist_corr=dCorr(spikesCoordinates(:,1:2),waveCenterPath(spikesCoordinates(:,3),:))
 
 %calculate spike projection on curve - spatial distance in each frame
 % spikeCurveCoordinates=zeros(size(spikesCoordinates));
@@ -112,6 +116,56 @@ plot(axisDataEndTime)
 
 imshow(squeeze(waveData(:,:,times(11))),[],'InitialMagnification','fit')
 imshow(visualizePath,'InitialMagnification','fit')
+
+
+%Gauss wave
+layoutSize=51*12;
+gaussSigma=51*3;
+% waveFrames=300;
+waveFrames=150;
+waveData=simulateGaussianWave(layoutSize,gaussSigma,waveFrames);
+pixelsPerChannel=[51 51];
+waveData=waveData(51:51:612,51:51:612,:);
+layoutSize=12;
+En=reshape(1:(layoutSize^2),layoutSize,layoutSize);
+
+exportVideo(waveData,'\\sil2\Literature\Projects\corplex\progress reports\meetings\next\Velocity profiles\Gauss Wave.avi',60,pixelsPerChannel);
+
+HT=hilbert(squeeze(convertMovieToChannels(waveData,En))').';
+HTabs=abs(HT);
+HTangle=angle(HT);
+
+[crossings,hilbertAmps] = getHilbertCrossings(HTabs,HTangle);
+
+startEndWave=[1 size(waveData,3)]; 
+
+plotCrossingsPhysical(crossings{1},startEndWave,flipud(En),hilbertAmps{1},'Units','frames')
+
+spikesCoordinates=simulateSpikes(waveData,0.004);
+plotWaveSpikes(spikesCoordinates,size(En));
+
+waveCenterPath = drawWavePath(crossings{1},hilbertAmps{1},startEndWave,flipud(En));
+exportVideo(waveData,'\\sil2\Literature\Projects\corplex\progress reports\meetings\next\Velocity profiles\Gauss Wave with Spikes and Wave Center Path1',30,pixelsPerChannel,'particlePath',waveCenterPath,'spikeCoordinates',spikesCoordinates);
+save('\\sil2\Literature\Projects\corplex\progress reports\meetings\next\Velocity profiles\waveSpikeCoordinates','spikesCoordinates')
+
+nSamples=size(waveCenterPath,1);
+t=1:nSamples;
+clear plottingInd
+plottingInd(1:nSamples)=1;
+plottingInd((nSamples+1):(nSamples+size(spikesCoordinates,1)))=2;
+h=plotWaveSpikes([waveCenterPath,t';spikesCoordinates],size(En),plottingInd);
+
+nSpikes=size(spikesCoordinates,1);
+nSamples=size(waveCenterPath,1);
+all_dists=sqrt((spikesCoordinates(:,1)-waveCenterPath(:,1)').^2+(spikesCoordinates(:,2)-waveCenterPath(:,2)').^2+(spikesCoordinates(:,3)*layoutSize/nSamples-linspace(1,layoutSize,nSamples)).^2);
+[dists,inds]=min(all_dists,[],2);
+% calculate curve length
+curveLocalLengths=sqrt((waveCenterPath(2:(end),1)-waveCenterPath(1:(end-1),1)).^2+(waveCenterPath(2:(end),2)-waveCenterPath(1:(end-1),2)).^2+(layoutSize/nSamples)^2); %layoutSize/nSamples frame is the normalized temporal distance between each point on the curve 
+curveSpeeds2=sqrt((waveCenterPath(2:(end),1)-waveCenterPath(1:(end-1),1)).^2+(waveCenterPath(2:(end),2)-waveCenterPath(1:(end-1),2)).^2);
+plot(curveSpeeds2)
+cureveLength=[0 cumsum(curveLocalLengths)'];
+scatter(cureveLength(inds),dists)
+h=plotWaveSpikes([waveCenterPath,t';spikesCoordinates],size(En),plottingInd);
 
 
 %open real data
