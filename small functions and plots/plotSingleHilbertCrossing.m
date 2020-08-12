6ee2be9cbb38c4984ae70234c721d54fb374c173
YuvalOrhser <u.vorsher@gmail.com>
2020-08-12 14:02:58 +0300

    vertical crossings offset, plot normalized data channels

--- small functions and plots/plotSingleHilbertCrossing.m
+++ small functions and plots/plotSingleHilbertCrossing.m
@@ -16,25 +16,47 @@ function [f] = plotSingleHilbertCrossing(singleCrossings,crossingsAmps,FD,crossi
 %      clusterLimits (nClusterX2)
 %           Plot also crossings clusters
 %      plotLegend (logical)
-%           True to plot legend (default), False for no legend
+%           True to plot legend (default), False for no legend.
+%           moreChannelsData overrides this to be false.
+%      CrossingsVerticalOffset (1x1)
+%           Move Crossings scatter up or down. Default is 0.
+%      moreChannelsData (nChXnSamples)
+%           Plot additional data in figure. Each channel will be normalized
+%           and plotted at the hight of the row number (channel i will be 
+%           below channel i+1). If this is given, plotLegend is set to
+%           false. Whoever's reading this is welcome to fix the legend it
+%           creates - I didn't have time.
+%      moreChannelsNumbers (nCX1) configures the vertical position of
+%      every channel in moreChannelsData (allows to plot less channels then
+%      total channel numbers, or to order it in different way).
 
 plotLegend=true;
+CrossingsVerticalOffset=0;
+plotAdditionalChannels=0;
 
 f=figure;
 for i=1:2:length(varargin)
    eval([varargin{i} '=varargin{' num2str(i+1) '};']);
 end
 
+if exist('moreChannelsData','var')
+    plotAdditionalChannels=1;
+    plotLegend=0;
+    if ~exist('moreChannelsNumbers','var')
+        moreChannelsNumbers=(1:size(moreChannelsData,1))';
+    end
+end
+
 chNum=1:size(singleCrossings,1);
 
 sz=25;
-h(1)=scatter(singleCrossings(chNum(1),singleCrossings(chNum(1),:)~=0),chNum(1)*ones(1,numel(singleCrossings(chNum(1),singleCrossings(chNum(1),:)~=0))),sz,squeeze(crossingsAmps(1,singleCrossings(chNum(1),:)~=0))');
+h(1)=scatter(singleCrossings(chNum(1),singleCrossings(chNum(1),:)~=0),chNum(1)*ones(1,numel(singleCrossings(chNum(1),singleCrossings(chNum(1),:)~=0)))+CrossingsVerticalOffset,sz,squeeze(crossingsAmps(1,singleCrossings(chNum(1),:)~=0))');
 hold on
 for i=chNum(2:end)
-        scatter(singleCrossings(i,singleCrossings(chNum(i),:)~=0),i*ones(1,numel(singleCrossings(i,singleCrossings(chNum(i),:)~=0))),sz,crossingsAmps(i,singleCrossings(chNum(i),:)~=0)'); %make crossingsAmp column vec to avoid RGB syntex in case there is only 3 element
+        scatter(singleCrossings(i,singleCrossings(chNum(i),:)~=0),i*ones(1,numel(singleCrossings(i,singleCrossings(chNum(i),:)~=0)))+CrossingsVerticalOffset,sz,crossingsAmps(i,singleCrossings(chNum(i),:)~=0)'); %make crossingsAmp column vec to avoid RGB syntex in case there is only 3 element
 end
 h(2)=plot(FD,'b');
-h(3)=plot(0:numel(FD),channelShown*ones(1,numel(FD)+1),'--k');
+h(3)=plot(0:numel(FD),channelShown*ones(1,numel(FD)+1)+CrossingsVerticalOffset,'--k');
 
 if plotLegend
 %legend hack
@@ -80,6 +102,24 @@ hcb=colorbar;
 title(hcb,'Hilbert Amplitude [uV]');
 xlabel('Samples')
 ylabel(['Filtered Data (Channel' num2str(channelShown) ') [uV]'])
+
+if plotAdditionalChannels
+    if size(moreChannelsNumbers,2)>1
+        warning('moreChannelsNumbers size should be nChX1!')
+    end
+    moreDataSamples=size(moreChannelsData,2);
+    %first normalize
+    moreChannelsData=moreChannelsData./(2*repmat(mean(moreChannelsData,2),1,moreDataSamples));
+    %add relevant height
+    moreChannelsData=moreChannelsData+repmat(moreChannelsNumbers,1,moreDataSamples);
+%     hLegend = findobj(gcf, 'Type', 'Legend');
+%     keepLegend=hLegend.String;
+%     nLegends=numel(hLegend.String);
+    plot(moreChannelsData');
+%     hLegend = findobj(gcf, 'Type', 'Legend');
+%     keepLegend=hLegend.String;
+%     legend(keepLegend(1:nLegends))
+end
 
 end
 
