c7ba8f935df43ca5b86df3044d3847d6ea9b6a99
YuvalOrhser <u.vorsher@gmail.com>
2020-08-16 13:45:45 +0300

    allow presetting number of channels nCh, predefine binSpikes as logical

--- small functions and plots/getSpikeBinMatByChannel.m
+++ small functions and plots/getSpikeBinMatByChannel.m
@@ -1,17 +1,20 @@
-function binSpikes = getSpikeBinMatByChannel(ticPath,start_times_ms,end_times_ms,sampling_rate)
+function binSpikes = getSpikeBinMatByChannel(ticPath,start_times_ms,end_times_ms,sampling_rate,nCh)
 %GETSPIKEBINMAT returns a nChXnSamples logical matrix with ones marking
 %spike times
 %   This function gets the spike times in t,ic format. It goes through all
 %   start_times and finds all spikes in time window from start_times(i) to 
 %   end_times(i) and marks ones in all the times(/samples) where each 
 %   channel fired. 
-%   nCh is defined by the last channel in ic (i.e. ic(1,end))
+%   If nCh is not given, it is taken to be the last channel in ic (i.e. ic(1,end))
 %   All trial spikes are concatenated to get a sequence in accordance to
 %   the matrices of getDataSequence.
 %   sampling_rate is samples/s.
 
 load(ticPath,'t','ic');
-nCh=ic(1,end);
+
+if isempty(nCh)
+    nCh=ic(1,end);
+end
 nTrials=numel(start_times_ms);
 
 ms2sample=sampling_rate/1000;
@@ -19,10 +22,10 @@ window_ms=end_times_ms(1)-start_times_ms(1);
 window_samples=round(window_ms*ms2sample);
 nSamples=window_samples*nTrials;
 
-binSpikes=zeros(nCh,nSamples);
+binSpikes=false(nCh,nSamples);
 
 
-for i=1:nCh
+for i=unique(ic(1,:))
     iChannelColumns=find(ic(1,:)==i);
 %     allChannelSpikes=[];
     for j=iChannelColumns 
@@ -30,10 +33,10 @@ for i=1:nCh
         for k=1:nTrials
             neuronSpikesInWindow=neuronSpikes(neuronSpikes>=start_times_ms(k) & neuronSpikes<=end_times_ms(k));
             neuronInd=round((neuronSpikesInWindow-start_times_ms(k))*ms2sample);
-            binSpikes(i,(k-1)*window_samples+neuronInd)=1;
+            binSpikes(i,(k-1)*window_samples+neuronInd)=true;
         end
     end
 end
-binSpikes=logical(binSpikes);
+
 end
 
