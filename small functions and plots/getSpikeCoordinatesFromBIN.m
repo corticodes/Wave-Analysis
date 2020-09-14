0e70eec0ab2c29731b9d7612c2507a9b5b7e67ec
YuvalOrhser <u.vorsher@gmail.com>
2020-09-14 10:51:11 +0300

    Fix wrong variable name (spikes->binSpikes)

--- small functions and plots/getSpikeCoordinatesFromBIN.m
+++ small functions and plots/getSpikeCoordinatesFromBIN.m
@@ -2,7 +2,7 @@ function [spikeCoordinates] = getSpikeCoordinatesFromBIN(binSpikes)
 %GETSPIKECOORDINATESFROMBIN returns the y,x,sample coordinates of spikes
 %given by the logical matrix binSpikes (yXxXsamples)
 %   spikeCoordinates are nSpikesX3, columns are y,x,sample
-    [y,x,t]=ind2sub(size(spikes),find(spikes));
+    [y,x,t]=ind2sub(size(binSpikes),find(binSpikes));
     spikeCoordinates=[y,x,t];
 end
 
