378367b0a742fd47720010b4cc7c28e14f1be457
yuvalorsher <61115964+yuvalorsher@users.noreply.github.com>
2020-09-29 13:40:01 +0300

    Create calcChannelsPosition.m

---
+++ small functions and plots/calcChannelsPosition.m
@@ -0,0 +1,16 @@
+function chPos = calcChannelsPosition(En)
+%CALCCHANNELSPOSITION calculates the position of channels according to the
+%electrode layout En
+%   INPUT:
+%       En - Electrode Layout
+%   OUTPUT:
+%   chPos - nChx2 of all channels' positions. chPos(i,1) and chPos (i,2)
+%   are the row and column of channel i respectively.
+nCh=max(En(:));
+
+chPos=zeros(nCh,2);
+for i=1:nCh
+    [chPos(i,1),chPos(i,2)]=find(En==i);
+end
+end
+
