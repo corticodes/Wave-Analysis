9d6f0cdebeacefedd16e11055cab42a10a330d8d
yuvalorsher <61115964+yuvalorsher@users.noreply.github.com>
2020-12-13 09:49:14 +0200

    return NaNs for rows without peaks (instead of end of row)

--- small functions and plots/getFirstPeaks.m
+++ small functions and plots/getFirstPeaks.m
@@ -1,6 +1,6 @@
 function [firstPeaksVal,firstPeaksLocs] = getFirstPeaks(A)
 %GETFIRSTPEAKS finds for each row in A the first local maximum using
-%Matlab's findpeaks
+%Matlab's findpeaks. If a row has no peak, getFirstPeaks returns a NaN
 %Todo: allow to change prominance
 
 rows=size(A,1);
@@ -9,6 +9,12 @@ firstPeaksLocs=zeros(rows,1);
 
 for i=1:rows
     [chpeaks,chlocs]=findpeaks(A(i,:));
+    if isempty(chpeaks)
+        chpeaks(1)=NaN;
+        chlocs(1)=NaN;
+%         chpeaks(1)=max(A(i,:));
+%         chlocs(1)=length(A(i,:));
+    end
     firstPeaksVal(i,1)=chpeaks(1);
     firstPeaksLocs(i,1)=chlocs(1);
 end
