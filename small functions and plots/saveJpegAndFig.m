275c6c230b3363da808a2960db6530f28227b85e
Yuval <corticodes@gmail.com>
2021-03-11 17:10:34 +0200

    add option to also export to eps

--- small functions and plots/saveJpegAndFig.m
+++ small functions and plots/saveJpegAndFig.m
@@ -1,4 +1,4 @@
-function [] = saveJpegAndFig(f,savePath,imgName,seperateFigDir)
+function [] = saveJpegAndFig(f,savePath,imgName,seperateFigDir,varargin)
 %SAVEJPEGANDFIG saves the figure f both as a matlab figure and a jpeg.
 %   INPUT:
 %       - f - handle of figure to save
@@ -8,6 +8,14 @@ function [] = saveJpegAndFig(f,savePath,imgName,seperateFigDir)
 %       - seperateFigDir - if true, saves the matlab fig file in a folder
 %       withing savePath called 'figs' (also, creates it if it doesn't
 %       exist). If false, saves both .fig and jpeg in savePath
+%       - possible varargins (given as 'Key',value pairs)
+%           - saveEPS - Also plots to EPS. Default is no. 
+
+saveEPS=0;
+
+for i=1:2:length(varargin)
+   eval([varargin{i} '=varargin{' num2str(i+1) '};']);
+end
 
 
 if seperateFigDir
@@ -20,6 +28,9 @@ else
 end
 
 saveas(f,[savePath imgName '.jpg'])
+if saveEPS
+    saveas(f,[savePath imgName],'epsc')
+end
 savefig(f,[figSaveDir imgName '.fig'])
 end
 
