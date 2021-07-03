b95a39c02d5f5618727391fc4d9b0e8b78c29212
Yuval <corticodes@gmail.com>
2021-07-03 13:18:24 +0300

    Add Figure settings, return axis

--- small functions and plots/plotCrossingsPhysical.m
+++ small functions and plots/plotCrossingsPhysical.m
@@ -1,4 +1,4 @@
-function [PLM,f] = plotCrossingsPhysical(selectedCrossings,startEndWave,En,hilbertAmps,varargin)
+function [PLM,f,ax] = plotCrossingsPhysical(selectedCrossings,startEndWave,En,hilbertAmps,varargin)
 %plotCrossingsPhysical uses IntensityPhysicalSpacePlot to plot the
 %time to the first crossing in selectedCrossings for each channel. It looks
 %at the window defined by startEndWave. t=0 is defined by the time that the 
@@ -20,6 +20,8 @@ function [PLM,f] = plotCrossingsPhysical(selectedCrossings,startEndWave,En,hilbe
 %       Figure title
 %   plotElectrodeNumbers (logical)
 %      Default is false
+%   plotColorBar (logical)
+%       Default is true
 %OUTPUT:
 %   PLM (1XnCh)
 %       Phase Latency Map - the times all channel reached the crossings
@@ -32,6 +34,17 @@ f=figure;
 Units='ms';
 plotElectrodeNumbers=false;
 plotSizeBar=0;
+plotColorBar=1;
+plotGridLines=0;
+figSizeUnits='centimeters';
+axesPosition=[2.14,0.34,0.19,1.9];
+hCbarFontSize=12;
+hCbarPosition=[13.8113    8.4667    5.9267    2.6458];
+hCbarYlabel={'Latency','[ms]'};
+hCbarTextPosition=[-4,0.5,0];
+hCbarFontUnits='points';
+hCbarYlablFontSize=8;
+markerSize=18;
 
 for i=1:2:length(varargin)
    eval([varargin{i} '=varargin{' num2str(i+1) '};']);
@@ -46,12 +59,16 @@ for i=chNum
     findCross=find(selectedCrossings(i,:)>=startEndWave(1) & selectedCrossings(i,:)<=startEndWave(2));
     if numel(findCross)==0
         pT(i)=NaN;
-        hAmp(i)=0;
+        if ~isempty(hilbertAmps)
+            hAmp(i)=0;
+        end
 %         i
     else
 %         if numel(findCross)>1 , i ,end %notify when a channel has more than 1 crossing within the window
         pT(i)=selectedCrossings(i,findCross(1))-startEndWave(1);
-        hAmp(i)=hilbertAmps(i,findCross(1));
+        if ~isempty(hilbertAmps)
+            hAmp(i)=hilbertAmps(i,findCross(1));
+        end
         channels(length(channels)+1)=i;
     end
 end
@@ -60,11 +77,21 @@ firstCrossing=min(pT);
 pT=(pT-firstCrossing);
 
 %plot the times of crossing in physical space
-% [hCbar]=IntensityPhysicalSpacePlot(chNum,pT,En,'plotElectrodeNumbers',0);
-[hCbar]=IntensityPhysicalSpacePlot(chNum,pT,En,'plotElectrodeNumbers',plotElectrodeNumbers,'markerSize',hAmp*60/max(hAmp),'plotSizeBar',plotSizeBar);
-% [hCbar]=IntensityPhysicalSpacePlot(chNum,pT,En,'plotElectrodeNumbers',0,'saturations',hAmp);
+if ~isempty(hilbertAmps)
+    [hCbar,ax]=IntensityPhysicalSpacePlot(chNum,pT,En,'plotElectrodeNumbers',plotElectrodeNumbers,'markerSize',hAmp*60/max(hAmp),'plotSizeBar',plotSizeBar,'plotColorBar',plotColorBar,'plotGridLines',plotGridLines);
+else
+    [hCbar,ax]=IntensityPhysicalSpacePlot(chNum,pT,En,'plotElectrodeNumbers',plotElectrodeNumbers,'markerSize',markerSize,'plotColorBar',plotColorBar,'plotGridLines',plotGridLines);
+end
 
-ylabel(hCbar,['Time From First Crossing [' Units ']']);
+set(gcf,'Units',figSizeUnits,'Position',figPosition);
+set(gca,'Units',figSizeUnits,'Position',axesPosition);
+
+if plotColorBar
+    hCbar.Units=figSizeUnits;
+    hCbar.FontSize=hCbarFontSize;
+    hCbar.Position=hCbarPosition;
+    ylabel(hCbar,hCbarYlabel,'Position',hCbarTextPosition,'FontUnits',hCbarFontUnits,'FontSize',hCbarYlablFontSize);
+end
 if exist('Title','var')
     title(Title)
 end 
