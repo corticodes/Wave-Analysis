403eb8d7a655eba5cb1fc15978eba5f4c1736cc7
Yuval <corticodes@gmail.com>
2021-07-03 13:19:26 +0300

    Add figure settings

--- small functions and plots/plotFilmstrip.m
+++ small functions and plots/plotFilmstrip.m
@@ -1,4 +1,4 @@
-function [f] = plotFilmstrip(data,flimstipStartEnd,En,waveChannelsPos,samplingFrequency)
+function [f] = plotFilmstrip(data,flimstripStartEnd,En,waveChannelsPos,samplingFrequency,varargin)
 %PLOTFILMSTRIP plots a filmstrip of the data in data, at fixed intervals
 %inside filmestripStartEnd. It also draws the elecrode positions described
 %in waveChannelPos
@@ -10,7 +10,24 @@ function [f] = plotFilmstrip(data,flimstipStartEnd,En,waveChannelsPos,samplingFr
 %   - En - electrode layout by which the frames are set up from data
 %   - waveChannelsPos (nElectrodesX2) - x,y positions of electrodes to
 %   plot. flipped ud.
+%   - possible varargin:
+%       colorBarWidthCM - width of colorbar in cm. default 0.25
+%       figPos - 'Position' value of the figure in cm
+%       plotTitleTimes - logical. Default is 0
+%       colorbarFontSize - default is 12
+%       boxEdgeSize - each frame edge size in cm. default 0.75
+%       boxPadding - padding between subplots. default is 0.25
 
+colorBarWidthCM=0.15;
+figPos=[19,14,7.3,2.3];
+plotTitleTimes=0;
+colorbarFontSize=12;
+boxEdgeSize=0.75; %cm
+boxPadding=0.25; %cm
+
+for i=1:2:length(varargin)
+   eval([varargin{i} '=varargin{' num2str(i+1) '};']);
+end
 
 if size(data,2)>1
    warning('Data array contains more than one trial!') 
@@ -18,54 +35,72 @@ end
 
 f=figure;
 
+
+
 numOfElectrodes=size(waveChannelsPos,1);
-representativeTimes=round(linspace(flimstipStartEnd(1),flimstipStartEnd(2),12)); %12 snippets
+representativeTimes=round(linspace(flimstripStartEnd(1),flimstripStartEnd(2),12)); %12 snippets
 movieData = convertChannelsToMovie(squeeze(data(:,1,:)),En);
 nRep=length(representativeTimes);
+
+n = ceil(nRep/2); % How many boxes in a row
+% ax = ones(2,n); %Handle so you can plot afterward
+
+
 minMovieVal=min(min(min(movieData(:,:,representativeTimes))));
 maxMovieVal=max(max(max(movieData(:,:,representativeTimes))));
-ax1=subplot(2,ceil(nRep/2),1);
-imagesc(ax1,movieData(:,:,representativeTimes(1)),[minMovieVal maxMovieVal])
+
+
+for i = 1:n
+    ax(1,i) = axes('Parent',f,'Units','centimeters','Position',[boxPadding+(boxPadding+boxEdgeSize)*(i-1) 2*boxPadding+boxEdgeSize boxEdgeSize boxEdgeSize],'Visible','off','XTick',[],'YTick',[]);
+    imagesc(movieData(:,:,representativeTimes(i)),[minMovieVal maxMovieVal])
+    set(gca,'xtick',[])
+    set(gca,'xticklabel',[])
+    set(gca,'ytick',[])
+    set(gca,'yticklabel',[])
+    if plotTitleTimes
+        title(['T=' num2str(round(representativeTimes(i)/samplingFrequency*1000)) 'ms'])
+    end
+    ax(2,i) = axes('Parent',f,'Units','centimeters','Position',[boxPadding+(boxPadding+boxEdgeSize)*(i-1) boxPadding boxEdgeSize boxEdgeSize],'Visible','off','XTick',[],'YTick',[]);
+    imagesc(movieData(:,:,representativeTimes(i+n)),[minMovieVal maxMovieVal])
+    set(gca,'xtick',[])
+    set(gca,'xticklabel',[])
+    set(gca,'ytick',[])
+    set(gca,'yticklabel',[])
+
+    if plotTitleTimes
+        title(['T=' num2str(round(representativeTimes(i)/samplingFrequency*1000)) 'ms'])
+    end
+end
+
+% imagesc(ax(1,1),movieData(:,:,representativeTimes(1)),[minMovieVal maxMovieVal])
 ax2 = axes;
-P = get(ax1,'Position');
-XLIM = get(ax1,'XLim');
-YLIM = get(ax1,'YLim');
-PA = get(ax1,'PlotBoxAspectRatio');
-set(ax2,'Position',P,'XLim',XLIM,'YLim',YLIM,'PlotBoxAspectRatio',PA)
-scatter(ax2,waveChannelsPos(:,1),size(En,1)+1-waveChannelsPos(:,2),25,linspace(0,1,numOfElectrodes),'filled')
+P = get(ax(1,1),'Position');
+XLIM = get(ax(1,1),'XLim');
+YLIM = get(ax(1,1),'YLim');
+PA = get(ax(1,1),'PlotBoxAspectRatio');
+set(ax2,'Units','centimeters','Position',P,'XLim',XLIM,'YLim',YLIM,'PlotBoxAspectRatio',PA)
+scatter(ax2,waveChannelsPos(:,1),size(En,1)+1-waveChannelsPos(:,2),10,linspace(0,1,numOfElectrodes),'filled')
 %%Link them together
-linkaxes([ax1,ax2])
+linkaxes([ax(1,1),ax2])
 %%Hide the top axes
-ax1.Visible = 'off';
-ax1.XTick = [];
-ax1.YTick = [];
 ax2.Visible = 'off';
 ax2.XTick = [];
 ax2.YTick = [];
+
 %%Give each one its own colormap
-colormap(ax1,'parula')
+colormap(ax(1,1),'parula')
 colormap(ax2,'jet')
 % %%Then add colorbars and get everything lined up
 % set([ax1,ax2],'Position',[.17 .11 .685 .815]);
 % cb1 = colorbar(ax1,'Position',[.05 .11 .0675 .815]);
 % cb2 = colorbar(ax2,'Position',[.88 .11 .0675 .815]);
-title(['T=' num2str(round(representativeTimes(1)/samplingFrequency*1000)) 'ms'])
 
-for i=2:nRep
-    subplot(2,ceil(nRep/2),i)
-    imagesc(movieData(:,:,representativeTimes(i)),[minMovieVal maxMovieVal])
-    title(['T=' num2str(round(representativeTimes(i)/samplingFrequency*1000)) 'ms'])
-    set(gca,'xtick',[])
-    set(gca,'xticklabel',[])
-    set(gca,'ytick',[])
-    set(gca,'yticklabel',[])
-end
 
-hp4 = get( subplot(2,ceil(nRep/2),i),'Position');
-colorbar('Position', [hp4(1)+hp4(3)+0.01  hp4(2)  0.01  hp4(2)+hp4(3)*2.1])
+set(f,'Units','centimeters','Position',figPos);
+% set(subplot(2,ceil(nRep/2),i),'Units','centimeters')
+hp4 = get( ax(2,end),'Position');
+colorbar(ax(1),'Units','centimeters','Position', [hp4(1)+hp4(3)+0.1  hp4(2)  colorBarWidthCM  hp4(2)+hp4(3)*2.1],'FontSize',colorbarFontSize)
 
-%     pos=get(gcf,'Position');
-set(gcf,'Position',[677   771   604   200]);
 
 
 end
